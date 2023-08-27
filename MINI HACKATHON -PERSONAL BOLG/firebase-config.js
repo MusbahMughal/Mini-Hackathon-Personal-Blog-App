f94a969da6c004d5e177a82f0c00d1dd2ee90385
Misbah Mughal <122009223+MusbahMughal@users.noreply.github.com>
2023-08-27 13:57:32 +0500

    Add files via upload

---
+++ MINI HACKATHON -PERSONAL BOLG/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAfmPT-sZ_R35K5fUSTio9xt8xCd7cNTsI",
+    authDomain: "my-personal-blog-app.firebaseapp.com",
+    projectId: "my-personal-blog-app",
+    storageBucket: "my-personal-blog-app.appspot.com",
+    messagingSenderId: "739070753909",
+    appId: "1:739070753909:web:6d45335bb5ddd1d6f78c3c",
+    measurementId: "G-1D7DQMES4B"
+  };
